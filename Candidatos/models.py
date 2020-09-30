826b2f28fb797f2737f1a9b0a60f94eb72106511
Johnny <urdin32@gmail.com>
2020-09-29 19:51:08 -0500

    Configuracion Inicial

--- Candidatos/models.py
+++ Candidatos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from Elecciones.models import PartidoPolitico
+
+DIGNIDAD=(
+    ("Presidente","Presidente"),
+    ("Asambleista Nacional","Asambleista Nacional"),
+    ("Asambleista Provincial","Asambleista Provincial"),
+    ("Parlamentario Andino","Parlamentario Andino")
+)
+
+class Dignidad(models.Model):
+    partido=models.ForeignKey(PartidoPolitico, on_delete=models.CASCADE)
+    dignidad=models.CharField(max_length=60, choices=DIGNIDAD)
+    nombre=models.CharField(max_length=60,null=True,blank=True)
