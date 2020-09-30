87bfe5e87e35bc14b92ecb49150abc626f85dad8
Johnny <urdin32@gmail.com>
2020-09-29 19:44:57 -0500

    Configuracion Inicial

--- Elecciones/admin.py
+++ Elecciones/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from CrtlElect.snippers import Attr
+from Elecciones.models import PartidoPolitico
+
+
+class Admin_PartidoPolitico(admin.ModelAdmin):
+    list_filter = Attr(PartidoPolitico)
+    list_display_links = Attr(PartidoPolitico)
