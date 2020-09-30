710a2bb95562e83412647cb62ef4c8f64c52f411
Johnny <urdin32@gmail.com>
2020-09-29 19:26:57 -0500

    Archivo Snnipers

---
+++ CrtlElect/snippers.py
@@ -0,0 +1,7 @@
+def get_filename(filename):
+    return filename.upper()
+
+
+def Attr(cls):
+    model = cls.__name__
+    return cls.__doc__.replace(model, "").replace("(", "").replace(")", "").replace(" ", "").split(",")
