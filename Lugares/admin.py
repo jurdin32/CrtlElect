ca1f11cdf85102ef264b64b125fa3561cc3565d0
Johnny <urdin32@gmail.com>
2020-09-29 20:22:53 -0500

    Configuracion Inicial

--- Lugares/admin.py
+++ Lugares/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from CrtlElect.snippers import Attr
+from Lugares.models import Provincia, Ciudad
+
+
+class CiudadInline(admin.StackedInline):
+    model = Ciudad
+    extra = 0
+
+@admin.register(Provincia)
+class Admin_Provincia(admin.ModelAdmin):
+    list_display = Attr(Provincia)
+    list_display_links = Attr(Provincia)
+    inlines = [CiudadInline]
+
+@admin.register(Ciudad)
+class Admin_Ciudad(admin.ModelAdmin):
+    list_display = Attr(Ciudad)
+    list_display_links = Attr(Ciudad)
--- Lugares/models.py
+++ Lugares/models.py
@@ -13,3 +13,6 @@ class Ciudad(models.Model):
 
     def __str__(self):
         return self.nombre
+
+    class Meta:
+        verbose_name_plural="Ciudades"
